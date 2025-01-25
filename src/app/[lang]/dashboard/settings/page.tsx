7ff3020cff072a19aea9921b8b25aafc4c3445ad
Jose Pablo Karam <karamjp7@gmail.com>
2025-01-25 04:24:07 -0600

    refactor(settings): remove unused getMultipleDictionaries call to simplify SettingsPage component

--- src/app/[lang]/dashboard/settings/page.tsx
+++ src/app/[lang]/dashboard/settings/page.tsx
@@ -1,23 +1,14 @@
 // src/app/[lang]/settings/page.tsx
-import { getMultipleDictionaries } from "@/utils/getDictionary";
 import type { Language } from "@/config/i18n";
 import { SettingsClient } from "./components/SettingsClient";
 import { getPageDictionary, settingsDictionary } from "@/utils/dictionary";
 
-// Server Component que solo obtiene los datos
 export default async function SettingsPage({
   params: { lang },
 }: {
   params: { lang: Language };
 }) {
-  // const { settings } = await getMultipleDictionaries(lang, [
-  //   "settings",
-  //   "common",
-  // ]);
-
-  const [dict] = await Promise.all([
-    getPageDictionary(settingsDictionary, lang),
-  ]);
+  const dict = await getPageDictionary(settingsDictionary, lang);
 
   return <SettingsClient dictionary={dict} />;
 }
