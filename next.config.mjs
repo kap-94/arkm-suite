52fcc7374de046e0eb1b2d9517d410805eb22682
arkm-studio <arkm.studio.web@gmail.com>
2025-03-06 18:18:53 -0600

    refactor(middleware): remove unused middleware files and simplify next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
-// next.config.mjs - Usando sintaxis ESM
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    // Asegúrate de que la configuración experimental sea compatible
-    experimental: {
-      // Puedes descomentar esto si necesitas paquetes externos específicos
-      // serverComponentsExternalPackages: [],
-    },
-    // Mejorar la información de depuración
-    logging: {
-      debug: true,
-      fetches: {
-        fullUrl: true,
-      },
-    },
-  };
-  
-  // Usando sintaxis de exportación ESM
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
